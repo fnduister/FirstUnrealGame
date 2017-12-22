17ec7fef1466441bd2a14d358998a00c1385e0c2
fabrice ndui <fndui@hotmail.com>
2017-12-22 11:31:07 -0500

    perfect

--- FirstUnrealGame/BallGame.cpp
+++ FirstUnrealGame/BallGame.cpp
@@ -2,6 +2,31 @@
 #include "BallGame.h"
 
 
+bool BallGame::isGameWon()
+{
+	return true;
+}
+
+bool BallGame::checkGuessValidity(std::string)
+{
+	return true;
+}
+
 void BallGame::reset()
 {
+}
+
+std::string BallGame::getGuess() const
+{
+	return m_guess;
+}
+
+int BallGame::getCurrentTry() const
+{
+	return m_currentTry;
+}
+
+int BallGame::getMaxTries() const
+{
+	return m_maxTries;
 }
--- FirstUnrealGame/BallGame.h
+++ FirstUnrealGame/BallGame.h
@@ -5,46 +5,24 @@ class BallGame
 {
 	std::string m_guess;
 	int m_maxTries;
-	int m_currentTries;
-	std::string m_name;
-	std::string m_base;
+	int m_currentTry;
 
 public:
 
-	BallGame(const std::string& guess, int maxTries, int currentTries, std::string name) :
+	BallGame(const std::string& guess, int maxTries, int currentTry, std::string name) :
 		m_guess(guess),
 		m_maxTries(maxTries),
-		m_currentTries(currentTries),
-		m_name(name)
+		m_currentTry(currentTry)
 	{
 	}
 
-	void reset();
-
-	std::string getGuess() const
-	{
-		return m_guess;
-	}
-
-	int getMaxTries() const
-	{
-		return m_maxTries;
-	}
-
-	int getCurrentTries() const  
-	{
-		return m_currentTries;
-	}
 	
-	std::string getName() const
-	{
-		return m_name;
-	}
-
-	void setName(const std::string& name)
-	{
-		m_name = name;
-	}
+	void reset();
+	std::string getGuess() const;
+	int getMaxTries() const;
+	int getCurrentTry() const;
+	bool isGameWon();
+	bool checkGuessValidity(std::string);
 
 private:
 	
