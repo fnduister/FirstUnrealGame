f26696a264d04b992f7bd362165e59d79af60fed
fabrice ndui <fndui@hotmail.com>
2017-12-21 21:16:51 -0500

    send ballgame header

---
+++ FirstUnrealGame/BallGame.cpp
@@ -0,0 +1,7 @@
+﻿#include "stdafx.h"
+#include "BallGame.h"
+
+
+void BallGame::reset()
+{
+}
---
+++ FirstUnrealGame/BallGame.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include <string>
+
+class BallGame
+{
+	std::string m_guess;
+	int m_maxTries;
+	int m_currentTries;
+	std::string m_name;
+	std::string m_base;
+
+public:
+
+	BallGame(const std::string& guess, int maxTries, int currentTries, std::string name) :
+		m_guess(guess),
+		m_maxTries(maxTries),
+		m_currentTries(currentTries),
+		m_name(name)
+	{
+	}
+
+	void reset();
+
+	std::string getGuess() const
+	{
+		return m_guess;
+	}
+
+	int getMaxTries() const
+	{
+		return m_maxTries;
+	}
+
+	int getCurrentTries() const  
+	{
+		return m_currentTries;
+	}
+	
+	std::string getName() const
+	{
+		return m_name;
+	}
+
+	void setName(const std::string& name)
+	{
+		m_name = name;
+	}
+
+private:
+	
+
+};
