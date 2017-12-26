5b7734a997eddb41e5c982e75b7f8ea39c2fb358
fabrice ndui <fndui@hotmail.com>
2017-12-25 22:31:49 -0500

    same old

--- FirstUnrealGame/BallGame.cpp
+++ FirstUnrealGame/BallGame.cpp
@@ -3,14 +3,39 @@
 #include <iostream>
 
 
+BallGame::BallGame(int maxTries) :
+	m_maxTries(maxTries)
+{
+	m_cows = 0;
+	m_bulls = 0;
+	m_currentTry = 0;
+}
+
+void BallGame::showResult()
+{
+
+	std::cout << "vous avez " << getCows() <<"cows et " << getBulls() << " bulls" << std::endl;
+}
+
+
+void BallGame::setCurrentTry()
+{
+	m_currentTry++;
+}
+
 bool BallGame::isGameWon()
 {
 	return true;
 }
 
-bool BallGame::isGameOver()
+bool BallGame::isGameOver() const
 {
-	return true;
+	if (m_currentTry >= m_maxTries)
+	{
+		std::cout << "desole vous avez depasse le nombre d'essaie";
+		return true;
+	}
+	return false;
 }
 
 bool BallGame::checkGuessValidity(std::string)
@@ -18,24 +43,38 @@ bool BallGame::checkGuessValidity(std::string)
 	return true;
 }
 
-void BallGame::reset()
-{
-}
-
-std::string BallGame::getGuess()
-{
-	std::cout << "enter your guess: ";
-	char guess[5];
-	std::cin >> guess;
-	std::cout << "your guess was : " << guess << std::endl;
-	setGuess(guess);
-	m_maxTries = 5;
-	return guess;
-}
-
 void BallGame::setGuess(std::string guess)
 {
 	m_guess = guess;
+};
+
+void BallGame::getGuess()
+{
+	std::cout << "essaie n: "<< getCurrentTry() << std::endl;
+	std::cout << "enter your guess: ";
+	std::string guess;
+	std::cin >> guess;
+	setGuess(guess);
+}
+
+unsigned BallGame::getCows() const
+{
+	return m_cows;
+}
+
+void BallGame::setCows(unsigned cows)
+{
+	m_cows = cows;
+}
+
+unsigned BallGame::getBulls() const
+{
+	return m_bulls;
+}
+
+void BallGame::setBulls(unsigned bulls)
+{
+	m_bulls = bulls;
 }
 
 void BallGame::reset()
@@ -46,4 +85,9 @@ void BallGame::reset()
 int BallGame::getMaxTries() const
 {
 	return m_maxTries;
+}
+
+int BallGame::getCurrentTry() const
+{
+	return m_currentTry;
 }
--- FirstUnrealGame/BallGame.h
+++ FirstUnrealGame/BallGame.h
@@ -6,23 +6,27 @@ class BallGame
 	std::string m_guess;
 	int m_maxTries;
 	int m_currentTry;
+	unsigned m_cows;
+	unsigned m_bulls;
 
 public:
 
-	BallGame(int maxTries, int currentTry = 0) :
-		m_maxTries(maxTries),
-		m_currentTry(currentTry)
-	{
-	}
-
-
+	explicit BallGame(int maxTries);
+	void showResult();
+	unsigned getCows() const;
+	void setCows(unsigned cows);
+	unsigned getBulls() const;
+	void setBulls(unsigned bulls);
 	static void reset();
-	std::string getGuess();
+	void getGuess();
 	void setGuess(std::string guess);
 	int getMaxTries() const;
+
 	int getCurrentTry() const;
+	void setCurrentTry();
+
 	bool isGameWon();
-	bool isGameOver();
+	bool isGameOver() const;
 	static bool checkGuessValidity(std::string);
 
 
